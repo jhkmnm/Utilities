088e68362782cdb1e17cff1bbe2aa004b8f3137c
jhkmnm <jhkmnm_123@163.com>
2016-11-30 18:47:47 +0800

    ff

---
+++ Utilities/DataVerifier.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Utilities
+{
+    [DebuggerStepThrough]
+    public class DataVerifier
+    {
+        private readonly StringBuilder content;
+        private Type customExceptionType;
+
+        public event Action<bool> Verified;
+
+        public DataVerifier()
+        {
+            this.content = new StringBuilder();
+            this.customExceptionType = null;
+            this.Pass = true;
+            this.PromptThrowException = false;
+        }
+
+        public DataVerifier(Type customExceptionType)
+            : this(true, customExceptionType, "", new object[0])
+        {
+        }
+
+        public DataVerifier(bool promptThrowException, Type customExceptionType)
+            : this(promptThrowException, customExceptionType, "", new object[0])
+        {
+        }
+
+        public DataVerifier(string title, params object[] args)
+            : this(false, null, title, args)
+        {
+        }
+
+        public DataVerifier(bool promptThrowException, Type customExceptionType, string title, params object[] args)
+            : this()
+        {
+            PromptThrowException = promptThrowException;
+            this.customExceptionType = customExceptionType;
+            Title = string.Format(title, args);
+        }
+
+        public void AddContent(string msg, params object[] args)
+        {
+            Check(true, msg, args);
+        }
+
+        public void Check(bool expression, string msg, params object[] args)
+        {
+            if (Pass)
+            {
+                Pass = !expression;
+            }
+            if (!(((msg.EndsWith("！") || msg.EndsWith("!")) || msg.EndsWith("。")) || msg.EndsWith(".")))
+            {
+                msg = msg + "！";
+            }
+            this.content.Append(expression ? (string.Format(msg, args) + "\r\n") : "");
+            this.RaiseVerified(expression);
+            if (this.PromptThrowException)
+            {
+                this.ThrowExceptionIfFailed();
+            }
+        }
+
+        public void Check(bool expression, Action beforeAction, string msg, params object[] args)
+        {
+            beforeAction();
+            this.Check(expression, msg, new object[0]);
+        }
+
+        public void CheckIfBeforePass(bool expression, string msg, params object[] args)
+        {
+            if (this.Pass)
+            {
+                this.Check(expression, msg, args);
+            }
+        }
+
+        public void CustomShowMsgIfFailed(Action<string> showMsgMethod)
+        {
+            if (!(this.Pass || (showMsgMethod == null)))
+            {
+                showMsgMethod(this.GetContent());
+            }
+        }
+
+        private string GetContent()
+        {
+            return (this.Title + "\r\n" + this.Content);
+        }
+
+        private void RaiseVerified(bool expression)
+        {
+            if (this.Verified != null)
+            {
+                this.Verified(expression);
+            }
+        }
+
+        public void ShowMsgIfFailed()
+        {
+            if (!this.Pass)
+            {
+                MessageBox.Show(this.GetContent(), @"提示", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+        }
+
+        public void ThrowExceptionIfFailed()
+        {
+            if (!this.Pass)
+            {
+                Exception exception = null;
+                if (this.CustomExceptionType == null)
+                {
+                    exception = new Exception(this.GetContent());
+                }
+                else
+                {
+                    exception = Activator.CreateInstance(this.CustomExceptionType, new object[] { this.GetContent() }) as Exception;
+                }
+                throw exception;
+            }
+        }
+
+        public string Content
+        {
+            get
+            {
+                return this.content.ToString();
+            }
+            set
+            {
+                this.content.AppendLine(value);
+            }
+        }
+
+        public Type CustomExceptionType
+        {
+            get
+            {
+                return this.customExceptionType;
+            }
+            set
+            {
+                if (!value.IsAssignableFrom(typeof(Exception)))
+                {
+                    throw new Exception("CustomExceptionType必须继承自Exception");
+                }
+                this.customExceptionType = value;
+            }
+        }
+
+        public bool Pass { get; private set; }
+
+        public bool PromptThrowException { get; set; }
+
+        public string Title { get; set; }
+    }
+}
---
+++ Utilities/ValueParseExtension.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Utilities
+{
+    public static class ValueParseExtension
+    {
+        /// <summary>
+        /// 转型成浮点型
+        /// 如果转型不成功则返回0
+        /// </summary>
+        /// <param name="oldValue">string类型的值</param>
+        /// <returns>float类型</returns>
+        public static float ToFloat(this string oldValue)
+        {
+            float newValue;
+            float.TryParse(oldValue, out newValue);
+            return newValue;
+        }
+
+        /// <summary>
+        /// 转型成浮点型
+        /// 如果转型不成功则返回默认值
+        /// </summary>
+        /// <param name="oldValue">string类型的值</param>
+        /// <param name="defaultValue">默认值</param>
+        /// <returns>float类型</returns>
+        public static float ToFloat(this string oldValue, float defaultValue)
+        {
+            float newValue;
+            if (float.TryParse(oldValue, out newValue))
+            {
+                return newValue;
+            }
+            else
+            {
+                return defaultValue;
+            }
+        }
+
+        /// <summary>
+        /// 转型成Int32类型
+        /// 如果转型不成功则返回0
+        /// </summary>
+        /// <param name="oldValue">string类型的值</param>
+        /// <returns>Int32类型</returns>
+        public static Int32 ToInt32(this string oldValue)
+        {
+            Int32 newValue;
+            Int32.TryParse(oldValue, out newValue);
+            return newValue;
+        }
+
+        /// <summary>
+        /// 转型成Int32类型
+        /// </summary>
+        /// <param name="oldValue">string类型的值</param>
+        /// <param name="defaultValue">默认值</param>
+        /// <returns>Int32类型</returns>
+        public static Int32 ToInt32(this string oldValue, Int32 defaultValue)
+        {
+            Int32 newValue;
+            if (Int32.TryParse(oldValue, out newValue))
+            {
+                return newValue;
+            }
+            else
+            {
+                return defaultValue;
+            }
+        }
+
+        /// <summary>
+        /// 转型成bool类型
+        /// </summary>
+        /// <param name="oldValue">string类型的值</param>
+        /// <returns>bool类型</returns>
+        public static bool ToBool(this string oldValue)
+        {
+            bool newValue;
+            bool.TryParse(oldValue, out newValue);
+            return newValue;
+        }
+
+        /// <summary>
+        /// 转型成bool类型
+        /// </summary>
+        /// <param name="oldValue">string类型的值</param>
+        /// <param name="defaultValue">默认值</param>
+        /// <returns>bool类型</returns>
+        public static bool ToBool(this string oldValue, bool defaultValue)
+        {
+            bool newValue;
+            if (bool.TryParse(oldValue, out newValue))
+            {
+                return newValue;
+            }
+            else
+            {
+                return defaultValue;
+            }
+        }
+
+        /// <summary>
+        /// 转型成double类型
+        /// </summary>
+        /// <param name="oldValue">string类型的值</param>
+        /// <returns>double类型</returns>
+        public static double ToDoubleNew(this string oldValue)
+        {
+            double newValue;
+            double.TryParse(oldValue, out newValue);
+            return newValue;
+        }
+
+        /// <summary>
+        /// 转型成double类型
+        /// </summary>
+        /// <param name="oldValue">string类型的值</param>
+        /// <param name="defaultValue">默认值</param>
+        /// <returns>double类型</returns>
+        public static double ToDoubleNew(this string oldValue, double defaultValue)
+        {
+            double newValue;
+            if (double.TryParse(oldValue, out newValue))
+            {
+                return newValue;
+            }
+            else
+            {
+                return defaultValue;
+            }
+        }
+
+        /// <summary>
+        /// 泛型转型，转型不成功将抛出FormatException异常
+        /// </summary>
+        /// <typeparam name="T">期待的类型</typeparam>
+        /// <param name="oldValue">string类型的值</param>
+        /// <returns>期待的类型</returns>
+        public static T ToGeneric<T>(this string oldValue)
+        {
+            try
+            {
+                T newValue = (T)Convert.ChangeType(oldValue, typeof(T));
+                return newValue;
+            }
+            catch (FormatException)
+            {
+                return default(T);
+            }
+        }
+
+        /// <summary>
+        /// 泛型转型，转型不成功将抛出FormatException异常
+        /// </summary>
+        /// <typeparam name="T">期待的类型</typeparam>
+        /// <param name="oldValue">string类型的值</param>
+        /// <returns>期待的类型</returns>
+        public static T ToGeneric<T>(this string oldValue, T defaultValue)
+        {
+            try
+            {
+                T newValue = (T)Convert.ChangeType(oldValue, typeof(T));
+                return newValue;
+            }
+            catch (FormatException)
+            {
+                return defaultValue;
+            }
+        }
+
+        /// <summary>
+        /// 转型成时间类型
+        /// </summary>
+        /// <param name="oldValue">string类型的值</param>
+        /// <returns>DateTime类型</returns>
+        public static DateTime ToDateTime(this string oldValue)
+        {
+            DateTime newValue;
+            DateTime.TryParse(oldValue, out newValue);
+            return newValue;
+        }
+
+        /// <summary>
+        /// 转型成时间类型
+        /// </summary>
+        /// <param name="oldValue">string类型的值</param>
+        /// <param name="defaultValue">默认值</param>
+        /// <returns>DateTime类型</returns>
+        public static DateTime ToDateTime(this string oldValue, DateTime defaultValue)
+        {
+            DateTime newValue;
+            if (DateTime.TryParse(oldValue, out newValue))
+            {
+                return newValue;
+            }
+            else
+            {
+                return defaultValue;
+            }
+        }
+    }
+}
